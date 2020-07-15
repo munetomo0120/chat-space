849eae625d375197b4277bd393c61b75f742e262
munetomo <fishmans0120@gmail.com>
2020-07-15 12:39:09 +0900

    add asynchronous communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function() {
+  function buildHTML(message) {
+    // if (MessageChannel.image) {
+    if (message.image) {
+      let html = `<div class="message-box">
+                    <div class="name-box">
+                      <div class="name-box__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="name-box__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message-content-box">
+                      <p class="inputted-message">
+                        ${message.content}
+                      </p>
+                      <img class="Message__image" src="${message.image}">
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      let html = `<div class="message-box">
+                    <div class="name-box">
+                      <div class="name-box__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="name-box__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message-content-box">
+                      <p class="inputted-message">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    }
+  }
+
+  $('.text-box').on('submit', function(e) {
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      let html = buildHTML(data);
+      $('.MessageField').append(html);
+      $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight });
+      $('form')[0].reset();
+      $('.submit-btn').attr('disabled', false);
+    })
+    .fail(function() {
+      alert('メッセージ送信に失敗しました');
+    })
+  });
+});
